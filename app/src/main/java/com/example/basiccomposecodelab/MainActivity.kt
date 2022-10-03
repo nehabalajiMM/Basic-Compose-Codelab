db3bc682b70eb51ea36b4de6c82266fd7a2ce747
nehabalaji <neha.balaji@mutualmobile.com>
2022-10-02 21:21:31 -0700

    modify the theme

--- app/src/main/java/com/example/basiccomposecodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiccomposecodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.basiccomposecodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiccomposecodelab.ui.theme.BasicComposeCodelabTheme
@@ -50,7 +52,12 @@ fun Greeting(name: String) {
                 .weight(1f)
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             OutlinedButton(onClick = {expanded.value = !expanded.value}) {
                 Text(if (expanded.value) "show less" else "show more")
@@ -59,6 +66,12 @@ fun Greeting(name: String) {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
